0f82b40327d8edcb480534b9137db91e4501f05d
Diogo010101 <90548278+Diogo010101@users.noreply.github.com>
2023-10-25 23:24:26 -0300

    desafia menu responsivo finalizado

---
+++ _modulo04/exercicios/ex_menu_responsivo/js/menu_drop.js
@@ -0,0 +1,10 @@
+var objMenu = document.querySelector("#menu_topo");
+
+
+function dropMenu(){
+    if(objMenu.style.display == 'none'){
+        objMenu.style.display = 'block'
+    }else{
+        objMenu.style.display = 'none'
+    }
+}
